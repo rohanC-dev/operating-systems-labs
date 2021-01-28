34e8d6f6312fdc0228a810b7964405317a368945
rohanC-dev <rohan.chopra241@gmail.com>
2021-01-28 16:21:00 -0500

    first version of lab2

--- lab2/lab2a/main.c
+++ lab2/lab2a/main.c
@@ -11,19 +11,60 @@ int main(int argc, char* argv[]) {
     char * person = "UNKNOWN";
     //Add code to change the exit_code depending on argc
     
+//    if(argc == 0){
+//        exit_code = 1;
+//    }else if(argc == 1){
+//        exit_code = 0; 
+//    }else if(argc > 1){
+//        exit_code = 2;
+//    }
+    
     
     //Add code to change 'person' if one is given on command line
+    int i, j;
+    char * strings[argc-1];
+    int stringCount = 0;
+    for(i = 1; i < argc; i++){
+        strings[stringCount] = argv[i];
+        stringCount++;
+    }
+    char helloString[] = {'h', 'e', 'l', 'l', 'o'};
+    for(i = 0; i < 1; i++){
+        for(j = 0; j < strlen(strings[i]); i++){
+            if(strings[i][j] == helloString[j]){
+                printf("true");
+            }
+        }
+//        if(strings[i] == helloString[i]){
+//            printf("true");
+//        }
+
+    }
+//    int len = strlen(argv[0]);
+//    char * last3 = argv[0] + len - 3; //last3 points to last 3 chars
+//    fprintf(stderr, "%s\n", argv[0]);
+//    fprintf(stderr, "%s\n", last3);
     
-    int len = strlen(argv[0]);
-    char * last3 = argv[0] + len - 3; //last3 points to last 3 chars
-    fprintf(stderr, "%s\n", argv[0]);
-    fprintf(stderr, "%s\n", last3);
+//    int len = strlen(argv[0]);
+//    char * last3 = argv[0] + len - 3; //last3 points to last 3 chars
+//    fprintf(stderr, "%s\n", argv[0]);
+//    fprintf(stderr, "%s\n", last3);
     
     //Add code to change 'greeting' if last 3 chars of command are 'bye'
   
-    printf("%s %s\n", greeting, person);
+    //printf("%s %s\n", greeting, person);
     
-    fprintf(stderr, "exit_code: %d\n", exit_code);
+    //fprintf(stderr, "exit_code: %d\n", exit_code);
+    
+    printf("\nargc value: %d", argc);
+//    printf("\nargv size: %d", sizeof(argv)/sizeof(argv[0]));
+//    printf("\nargv value: %s", argv[0]);
+//    printf("\nargv value: %c", *argv[1]);
+//    printf("\nargv value: %s", argv[2]);
+    //printf("\nargv value: %s", strings[0]);
+    printf("\nargv value: %c", strings[0][1]);
+    //printf("\nargc value: %d", strlen(strings[0]));
+    
     
     return exit_code; //Could also say exit(exit_code)
 }
--- lab2/lab2a/nbproject/Makefile-Debug.mk
+++ lab2/lab2a/nbproject/Makefile-Debug.mk
@@ -21,8 +21,8 @@ FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=GNU-Linux
-CND_DLIB_EXT=so
+CND_PLATFORM=Cygwin-Windows
+CND_DLIB_EXT=dll
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -56,13 +56,13 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lab2a
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lab2a.exe
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lab2a: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lab2a.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lab2a ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/main.o: main.c 
+${OBJECTDIR}/main.o: main.c
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.c) -g -std=c99 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.c
@@ -73,7 +73,6 @@ ${OBJECTDIR}/main.o: main.c
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lab2a
 
 # Subprojects
 .clean-subprojects:
